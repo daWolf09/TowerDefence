e2817526478f52fa3048db019862ffa2410fcf07
Lema111 <110460422+Lema111@users.noreply.github.com>
2023-08-31 22:10:01 +0200

    Update TowerDenfece_solidtiy.sol

--- TowerDenfece_solidtiy.sol
+++ TowerDenfece_solidtiy.sol
@@ -16,8 +16,8 @@ contract TowerDefenseGame {
     mapping(string => uint256) public enemyHealth;
     mapping(string => mapping(string => uint256)) private baseAttacks;
     mapping(string => string[]) public baseWaypoints;
-
     mapping(string => string) public waypoints;
+    mapping(address => bool) public baseBuilds;
 
     uint256 public baseEnemyHealth = START_ZOMBIE_HEALTH;
 
@@ -31,12 +31,67 @@ contract TowerDefenseGame {
         playerBalance = 100;
 
         baseWaypoints["Base1"] = ["W1", "W2"];
-        // ... Fügen Sie die Initialisierung der anderen Waypoints hinzu ...
+        baseWaypoints["Base2"] = ["W1", "W2"];
+        baseWaypoints["Base3"] = ["W1", "W2", "W3"];
+        baseWaypoints["Base4"] = ["W1", "W2", "W3"];
+        baseWaypoints["Base5"] = ["W2", "W3", "W4"];
+        baseWaypoints["Base6"] = ["W3", "W4", "W5"];
+        baseWaypoints["Base7"] = ["W3", "W4"];
+        baseWaypoints["Base8"] = ["W5", "W6", "W7"];
+        baseWaypoints["Base9"] = ["W4", "W5"];
+        baseWaypoints["Base10"] = ["W6", "W7", "W8"];
+        baseWaypoints["Base11"] = ["W4", "W5", "W6"];
+        baseWaypoints["Base12"] = ["W7", "W8", "W9"];
+        baseWaypoints["Base13"] = ["W5", "W6", "W7"];
+        baseWaypoints["Base14"] = ["W8", "W9", "W10"];
+        baseWaypoints["Base15"] = ["W6", "W7", "W8"];
+        baseWaypoints["Base16"] = ["W9", "W10"];
+        baseWaypoints["Base17"] = ["W7", "W8", "W9"];
+        baseWaypoints["Base18"] = ["W20", "W21", "W23", "W20", "W19", "W24", "W25"];
+        baseWaypoints["Base19"] = ["W9" , "W10" , "W11"];
+        baseWaypoints["Base20"] = ["W18", "W19", "W20", "W24", "W25", "W26"];
+        baseWaypoints["Base21"] = ["W11", "W12", "W13"];
+        baseWaypoints["Base22"] = ["W17", "W18", "W19", "W25", "W26", "W27"];
+        baseWaypoints["Base23"] = ["W12", "W13", "W14"];
+        baseWaypoints["Base24"] = ["W16", "W17", "W18", "W26", "W27", "W28"];
+        baseWaypoints["Base25"] = ["W14", "W15", "W16"];
+        baseWaypoints["Base26"] = ["W15", "W16", "W17", "W27", "W28", "W29"];
+        baseWaypoints["Base27"] = ["W16", "W17", "W18"];
+        baseWaypoints["Base28"] = ["W15", "W16", "W28", "W29"];
+        baseWaypoints["Base29"] = ["W17", "W18", "W19"];
+        baseWaypoints["Base30"] = ["W29", "W30"];
+        baseWaypoints["Base31"] = ["W18", "W19", "W20"];
+        baseWaypoints["Base32"] = ["W29", "W30", "W31"];
+        baseWaypoints["Base33"] = ["W19", "W20", "W21"];
+        baseWaypoints["Base34"] = ["W30", "W31", "W32"];
+        baseWaypoints["Base35"] = ["W20", "W21"];
+        baseWaypoints["Base36"] = ["W31", "W32", "W33"];
+        baseWaypoints["Base37"] = ["W21", "W22"];
+        baseWaypoints["Base38"] = ["W32", "W33", "W34"];
+        baseWaypoints["Base39"] = ["W21", "W22", "W23"];
+        baseWaypoints["Base40"] = ["W33", "W34"];
+        baseWaypoints["Base41"] = ["W22", "W23"];
+        baseWaypoints["Base42"] = ["W34", "W35"];
+        baseWaypoints["Base43"] = ["W23", "W24"];
+        baseWaypoints["Base44"] = ["W34", "W35", "W36"];
+        baseWaypoints["Base45"] = ["W23", "W24", "W25"];
+        baseWaypoints["Base46"] = ["W35", "W36", "W37"];
+        baseWaypoints["Base47"] = ["W24", "W25", "W26"];
+        baseWaypoints["Base48"] = ["W36", "W37"];
+        baseWaypoints["Base49"] = ["W25", "W26", "W27"];
+        baseWaypoints["Base51"] = ["W26", "W27", "W28"];
+        baseWaypoints["Base53"] = ["W28", "W29", "W30"];
+        baseWaypoints["Base55"] = ["W30", "W31", "W32"];
+        baseWaypoints["Base57"] = ["W31", "W32", "W33"];
+        baseWaypoints["Base59"] = ["W33", "W34", "W35"];
+        baseWaypoints["Base61"] = ["W35", "W36", "W37"];
+        baseWaypoints["Base63"] = ["W36", "W37"];
+        
 
-        for (uint256 i = 0; i < baseWaypoints["Base1"].length; i++) {
-            for (uint256 j = 1; j <= 44; j++) {
-                string memory baseType = string(abi.encodePacked("Base", uintToString(j)));
-                baseAttacks[baseType][baseWaypoints["Base1"][i]] = 10;
+            for (uint256 i = 1; i <= 55; i++) {
+            string memory baseType = string(abi.encodePacked("Base", uintToString(i)));
+            for (uint256 j = 0; j < baseWaypoints[baseType].length; j++) {
+                baseAttacks[baseType][baseWaypoints[baseType][j]] = 10;
             }
         }
     }
@@ -64,10 +119,10 @@ contract TowerDefenseGame {
 
     function startWave() external gameInProgress noWaveInProgress {
         require(isWaveButtonEnabled, "Next Wave button is disabled");
-        
+        require(baseBuilds[msg.sender], "You must build a base first");
         
         isWaveInProgress = true;
-        isWaveButtonEnabled = false;
+        isWaveButtonEnabled = false; // Deaktiviere den Button während der Wave
     }
 
     function updateWaveDifficulty() internal {
@@ -115,6 +170,10 @@ contract TowerDefenseGame {
             }
             enemyHealth[enemyType] = 0;
         }
+    }
+    function buildBase() external gameInProgress {
+        require(!baseBuilds[msg.sender], "You already built a base");
+        baseBuilds[msg.sender] = true;
     }
 
     function paybackToOwner() external onlyOwner {
