09ac7dff6ef4ccfe0200e1d77eed21d42071dca7
Lema111 <110460422+Lema111@users.noreply.github.com>
2023-09-02 10:06:19 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -131,7 +131,27 @@ function App() {
       }
     }
   }
+  function getButtonPositions() {
+    const buttonElements = document.querySelectorAll('.BuildButton'); // Wählen Sie alle Buttons mit der Klasse "BuildButton"
+    const positions = [];
+  
+    buttonElements.forEach((button) => {
+      const id = button.id.split('-')[1]; // Extrahieren Sie die ID des Buttons aus der ID im Format "BuildButton-X"
+      const style = getComputedStyle(button); // Holen Sie sich die berechneten CSS-Stile des Buttons
+  
+      const x = parseInt(style.left, 10); // Extrahieren Sie die X-Position aus den CSS-Stilen und wandeln Sie sie in eine ganze Zahl um
+      const y = parseInt(style.top, 10); // Extrahieren Sie die Y-Position aus den CSS-Stilen und wandeln Sie sie in eine ganze Zahl um
+  
+      positions.push({ id: parseInt(id, 10), x, y });
+    });
+  
 
+  }
+  
+  // Beispielaufruf der Funktion und Ausgabe in der Konsole
+  const buttonPositions = getButtonPositions();
+  console.log(buttonPositions);
+  
 
   return (
     <div className="container">
@@ -144,69 +164,69 @@ function App() {
         <button className="startFlyingButton" onClick={() => zombiestart(10000)}>
           Start Flying
         </button>
-        <button  className="BuildButton1" onClick={() => Base_Build_request(1)}></button>
-        <button className="BuildButton2" onClick={() => Base_Build_request(2)}></button>
-        <button className="BuildButton3" onClick={() => Base_Build_request(3)}></button>
-        <button className="BuildButton4" onClick={() => Base_Build_request(4)}></button>
-        <button className="BuildButton5" onClick={() => Base_Build_request(5)}></button>
-        <button className="BuildButton6" onClick={() => Base_Build_request(6)}></button>
-        <button className="BuildButton7" onClick={() => Base_Build_request(7)}></button>
-        <button className="BuildButton8" onClick={() => Base_Build_request(8)}></button>
-        <button className="BuildButton9" onClick={() => Base_Build_request(9)}></button>
-        <button className="BuildButton10" onClick={() => Base_Build_request(10)}></button>
-        <button className="BuildButton11" onClick={() => Base_Build_request(11)}></button>
-        <button className="BuildButton12" onClick={() => Base_Build_request(12)}></button>
-        <button className="BuildButton13" onClick={() => Base_Build_request(13)}></button>
-        <button className="BuildButton14" onClick={() => Base_Build_request(14)}></button>
-        <button className="BuildButton15" onClick={() => Base_Build_request(15)}></button>
-        <button className="BuildButton16" onClick={() => Base_Build_request(16)}></button>
-        <button className="BuildButton17" onClick={() => Base_Build_request(17)}></button>
-        <button className="BuildButton18" onClick={() => Base_Build_request(18)}></button>
-        <button className="BuildButton19" onClick={() => Base_Build_request(19)}></button>
-        <button className="BuildButton20" onClick={() => Base_Build_request(20)}></button>
-        <button className="BuildButton21" onClick={() => Base_Build_request(21)}></button>
-        <button className="BuildButton22" onClick={() => Base_Build_request(22)}></button>
-        <button className="BuildButton23" onClick={() => Base_Build_request(23)}></button>
-        <button className="BuildButton24" onClick={() => Base_Build_request(24)}></button>
-        <button className="BuildButton25" onClick={() => Base_Build_request(25)}></button>
-        <button className="BuildButton26" onClick={() => Base_Build_request(26)}></button>
-        <button className="BuildButton27" onClick={() => Base_Build_request(27)}></button>
-        <button className="BuildButton28" onClick={() => Base_Build_request(28)}></button>
-        <button className="BuildButton29" onClick={() => Base_Build_request(29)}></button>
-        <button className="BuildButton30" onClick={() => Base_Build_request(30)}></button>
-        <button className="BuildButton31" onClick={() => Base_Build_request(31)}></button>
-        <button className="BuildButton32" onClick={() => Base_Build_request(32)}></button>
-        <button className="BuildButton33" onClick={() => Base_Build_request(33)}></button>
-        <button className="BuildButton34" onClick={() => Base_Build_request(34)}></button>
-        <button className="BuildButton35" onClick={() => Base_Build_request(35)}></button>
-        <button className="BuildButton36" onClick={() => Base_Build_request(36)}></button>
-        <button className="BuildButton37" onClick={() => Base_Build_request(37)}></button>
-        <button className="BuildButton38" onClick={() => Base_Build_request(38)}></button>
-        <button className="BuildButton39" onClick={() => Base_Build_request(39)}></button>
-        <button className="BuildButton40" onClick={() => Base_Build_request(40)}></button>
-        <button className="BuildButton41" onClick={() => Base_Build_request(41)}></button>
-        <button className="BuildButton42" onClick={() => Base_Build_request(42)}></button>
-        <button className="BuildButton43" onClick={() => Base_Build_request(43)}></button>
-        <button className="BuildButton44" onClick={() => Base_Build_request(44)}></button>
-        <button className="BuildButton45" onClick={() => Base_Build_request(45)}></button>
-        <button className="BuildButton46" onClick={() => Base_Build_request(46)}></button>
-        <button className="BuildButton47" onClick={() => Base_Build_request(47)}></button>
-        <button className="BuildButton48" onClick={() => Base_Build_request(48)}></button>
-        <button className="BuildButton49" onClick={() => Base_Build_request(49)}></button>
+        <button id="BuildButton1" className="BuildButton1" onClick={() => Base_Build_request(1)}></button>
+        <button id="BuildButton2" className="BuildButton2" onClick={() => Base_Build_request(2)}></button>
+        <button id="BuildButton3" className="BuildButton3" onClick={() => Base_Build_request(3)}></button>
+        <button id="BuildButton4" className="BuildButton4" onClick={() => Base_Build_request(4)}></button>
+        <button id="BuildButton5" className="BuildButton5" onClick={() => Base_Build_request(5)}></button>
+        <button id="BuildButton6" className="BuildButton6" onClick={() => Base_Build_request(6)}></button>
+        <button id="BuildButton7" className="BuildButton7" onClick={() => Base_Build_request(7)}></button>
+        <button id="BuildButton8" className="BuildButton8" onClick={() => Base_Build_request(8)}></button>
+        <button id="BuildButton9" className="BuildButton9" onClick={() => Base_Build_request(9)}></button>
+        <button id="BuildButton10" className="BuildButton10" onClick={() => Base_Build_request(10)}></button>
+        <button id="BuildButton11" className="BuildButton11" onClick={() => Base_Build_request(11)}></button>
+        <button id="BuildButton12" className="BuildButton12" onClick={() => Base_Build_request(12)}></button>
+        <button id="BuildButton13" className="BuildButton13" onClick={() => Base_Build_request(13)}></button>
+        <button id="BuildButton14" className="BuildButton14" onClick={() => Base_Build_request(14)}></button>
+        <button id="BuildButton15" className="BuildButton15" onClick={() => Base_Build_request(15)}></button>
+        <button id="BuildButton16" className="BuildButton16" onClick={() => Base_Build_request(16)}></button>
+        <button id="BuildButton17" className="BuildButton17" onClick={() => Base_Build_request(17)}></button>
+        <button id="BuildButton18" className="BuildButton18" onClick={() => Base_Build_request(18)}></button>
+        <button id="BuildButton19" className="BuildButton19" onClick={() => Base_Build_request(19)}></button>
+        <button id="BuildButton20" className="BuildButton20" onClick={() => Base_Build_request(20)}></button>
+        <button id="BuildButton21" className="BuildButton21" onClick={() => Base_Build_request(21)}></button>
+        <button id="BuildButton22" className="BuildButton22" onClick={() => Base_Build_request(22)}></button>
+        <button id="BuildButton23" className="BuildButton23" onClick={() => Base_Build_request(23)}></button>
+        <button id="BuildButton24" className="BuildButton24" onClick={() => Base_Build_request(24)}></button>
+        <button id="BuildButton25" className="BuildButton25" onClick={() => Base_Build_request(25)}></button>
+        <button id="BuildButton26" className="BuildButton26" onClick={() => Base_Build_request(26)}></button>
+        <button id="BuildButton27" className="BuildButton27" onClick={() => Base_Build_request(27)}></button>
+        <button id="BuildButton28" className="BuildButton28" onClick={() => Base_Build_request(28)}></button>
+        <button id="BuildButton29" className="BuildButton29" onClick={() => Base_Build_request(29)}></button>
+        <button id="BuildButton30" className="BuildButton30" onClick={() => Base_Build_request(30)}></button>
+        <button id="BuildButton31" className="BuildButton31" onClick={() => Base_Build_request(31)}></button>
+        <button id="BuildButton32" className="BuildButton32" onClick={() => Base_Build_request(32)}></button>
+        <button id="BuildButton33" className="BuildButton33" onClick={() => Base_Build_request(33)}></button>
+        <button id="BuildButton34" className="BuildButton34" onClick={() => Base_Build_request(34)}></button>
+        <button id="BuildButton35" className="BuildButton35" onClick={() => Base_Build_request(35)}></button>
+        <button id="BuildButton36" className="BuildButton36" onClick={() => Base_Build_request(36)}></button>
+        <button id="BuildButton37" className="BuildButton37" onClick={() => Base_Build_request(37)}></button>
+        <button id="BuildButton38" className="BuildButton38" onClick={() => Base_Build_request(38)}></button>
+        <button id="BuildButton39" className="BuildButton39" onClick={() => Base_Build_request(39)}></button>
+        <button id="BuildButton40" className="BuildButton40" onClick={() => Base_Build_request(40)}></button>
+        <button id="BuildButton41" className="BuildButton41" onClick={() => Base_Build_request(41)}></button>
+        <button id="BuildButton42" className="BuildButton42" onClick={() => Base_Build_request(42)}></button>
+        <button id="BuildButton43" className="BuildButton43" onClick={() => Base_Build_request(43)}></button>
+        <button id="BuildButton44" className="BuildButton44" onClick={() => Base_Build_request(44)}></button>
+        <button id="BuildButton45" className="BuildButton45" onClick={() => Base_Build_request(45)}></button>
+        <button id="BuildButton46" className="BuildButton46" onClick={() => Base_Build_request(46)}></button>
+        <button id="BuildButton47" className="BuildButton47" onClick={() => Base_Build_request(47)}></button>
+        <button id="BuildButton48" className="BuildButton48" onClick={() => Base_Build_request(48)}></button>
+        <button id="BuildButton49" className="BuildButton49" onClick={() => Base_Build_request(49)}></button>
 
-        <button className="BuildButton51" onClick={() => Base_Build_request(51)}></button>
+        <button id="BuildButton51" className="BuildButton51" onClick={() => Base_Build_request(51)}></button>
 
-        <button className="BuildButton53" onClick={() => Base_Build_request(53)}></button>
+        <button id="BuildButton53" className="BuildButton53" onClick={() => Base_Build_request(53)}></button>
 
-        <button className="BuildButton55" onClick={() => Base_Build_request(55)}></button>
+        <button id="BuildButton55" className="BuildButton55" onClick={() => Base_Build_request(55)}></button>
 
-        <button className="BuildButton57" onClick={() => Base_Build_request(57)}></button>
+        <button id="BuildButton57" className="BuildButton57" onClick={() => Base_Build_request(57)}></button>
 
-        <button className="BuildButton59" onClick={() => Base_Build_request(59)}></button>
+        <button id="BuildButton59" className="BuildButton59" onClick={() => Base_Build_request(59)}></button>
 
-        <button className="BuildButton61" onClick={() => Base_Build_request(61)}></button>
+        <button id="BuildButton61" className="BuildButton61" onClick={() => Base_Build_request(61)}></button>
 
-        <button className="BuildButton63" onClick={() => Base_Build_request(63)}></button>
+        <button id="BuildButton63" className="BuildButton63" onClick={() => Base_Build_request(63)}></button>
 
 
       </div>
